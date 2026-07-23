80808a5a9ef48cd09644bbd2a1a5dec6fc911a9a
hanssantos02 <hansprielasantos02@gmail.com>
2026-07-23 20:14:09 +0800

    Add reparent camera
    fix: reparent camera to dropped weapon to prevent zoom-out on death

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed: float = 100.0
 @export var dropped_weapon_scene: PackedScene
 @onready var weapon_pivot: Node2D = $WeaponPivot
+@onready var camera_2d: Camera2D = $Camera2D
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -11,6 +12,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_health_component_died() -> void:
 	var weapon = dropped_weapon_scene.instantiate()
+	camera_2d.reparent(weapon)
 	weapon.stats = weapon_pivot.stats
 	weapon.global_position = global_position
 	get_tree().current_scene.add_child(weapon)
