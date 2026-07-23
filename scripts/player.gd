c3ba7837d19c5d4d3da8198261aa19d13806e9be
hanssantos02 <hansprielasantos02@gmail.com>
2026-07-23 20:10:47 +0800

    Implement weapon drop logic
    feat: implement weapon drop and stat transfer on player death

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @export var speed: float = 100.0
+@export var dropped_weapon_scene: PackedScene
+@onready var weapon_pivot: Node2D = $WeaponPivot
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -8,4 +10,9 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_health_component_died() -> void:
-	print("The Ancestor has fallen!")
+	var weapon = dropped_weapon_scene.instantiate()
+	weapon.stats = weapon_pivot.stats
+	weapon.global_position = global_position
+	get_tree().current_scene.add_child(weapon)
+	queue_free()
+	
