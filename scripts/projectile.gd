c67bbd2f2eba95c72d39d743253fbf4119fb7c86
hanssantos02 <hansprielasantos02@gmail.com>
2026-07-23 19:55:15 +0800

    feat: implement projectile damage
    feat: implement projectile damage via enemy hitbox detection

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -5,3 +5,11 @@ var damage: float
 
 func _process(delta: float) -> void:
 	global_position += transform.x * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("enemy"):
+		return
+	for child in body.get_children():
+		if child is HealthComponent:
+			child.take_damage(damage)
+			queue_free()
