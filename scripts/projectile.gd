43205ba37b2d1b5d503f9abdf552735fbbc96d16
hanssantos02 <hansprielasantos02@gmail.com>
2026-07-23 12:53:14 +0800

    feat: create WeaponStats resource
    feat: create WeaponStats custom resource architecture

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -2,7 +2,6 @@ extends Area2D
 
 @export var speed: float = 300.0
 
-var direction: Vector2 = Vector2.RIGHT
 
 func _process(delta: float) -> void:
-	global_position += direction * speed * delta
+	global_position += transform.x * speed * delta
---
+++ scripts/weapon_stats.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name WeaponStats
+
+@export var damage: float = 10.0
+@export var fire_rate: float = 0.5
+@export var projectile_speed: float = 300.0
