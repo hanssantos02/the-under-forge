0be0bdd5e8df43d9cbd352b25d5ec96917082e7f
hanssantos02 <hansprielasantos02@gmail.com>
2026-07-24 12:02:47 +0800

    Update dropped weapon
    Updated the dropped weapon scripts to make it look like it's floating upon death

--- scripts/dropped_weapon.gd
+++ scripts/dropped_weapon.gd
@@ -2,6 +2,12 @@ extends Area2D
 
 @export var stats: WeaponStats
 
+func _ready() -> void:
+	var tween = create_tween().set_loops()
+	var start_y = position.y
+	tween.tween_property(self, "position:y", start_y - 5.0, 1.0).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(self, "position:y", start_y, 1.0).set_trans(Tween.TRANS_SINE)
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		body.equip_weapon(stats)
